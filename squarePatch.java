a64d8fb3e0ed22bb4f29ac3ba170f14fa5dd0a7e
Mohamed Sassi <msassi@wp.pl>
2021-04-05 13:41:32 +0200

    commit after adding squarepatch class

---
+++ squarePatch.java
@@ -0,0 +1,27 @@
+// https://edabit.com/challenge/7Tb7qMDQHtz3xpydd
+// Create a function that takes an integer and outputs an n x n square 
+// solely consisting of the integer n.
+
+public class squarePatch{
+
+static void printMatrix(int n){
+if(n==0) System.out.printf("[]%n");    
+for(int i=0;i<n;i++){
+    System.out.printf("[");
+    for(int j=0;j<n-1;j++){
+        System.out.printf("%d,",n);
+    }
+    if (i < n-1) System.out.printf("%d],%n",n);
+    else System.out.printf("%d]%n",n);
+
+}
+}
+public static void main(String[] args) {
+    printMatrix(0);
+    printMatrix(1);
+    printMatrix(2);
+    printMatrix(3);
+    printMatrix(4);
+    printMatrix(5);
+}
+}
---
+++ triangle30and60.java
@@ -0,0 +1,28 @@
+//https://edabit.com/challenge/J26bZ6Fv6bWEisDYj?fbclid=IwAR0y55yEQsPdU4ZGHCPYmrlUNmb3OYALlHG-KLd8HOuRZet5ssbTJCA6zsg
+/*Given the shortest side of a 30° by 60° by 90° triangle 
+you have to find out the other 2 sides,
+ (return the longest side, medium-length side).
+ otherSides(1) ➞ [2.0, 1.73]
+otherSides(12) ➞ [24.0, 20.0]
+otherSides(2) ➞ [4.0, 3.46]
+otherSides(3)➞ [6.0, 5.2]
+ */
+import java.lang.Math;
+public class triangle30and60 {
+    static double[] otherSiders(double a){
+        double b;
+        double c;
+        b=a*Math.sqrt(3);
+        c=2*a;
+        double[] d = new double[2];
+        d[0]=b;
+        d[1]=c;
+        return d;
+}
+        public static void main(String[] args) {
+        System.out.printf("otherSiders(1)---> [ %.2f , %.2f ] %n",otherSiders(1)[1],otherSiders(1)[0]);
+        System.out.printf("otherSiders(12)---> [ %.2f , %.2f ] %n",otherSiders(12)[1],otherSiders(12)[0]);
+        System.out.printf("otherSiders(2)---> [ %.2f , %.2f ] %n",otherSiders(2)[1],otherSiders(2)[0]);
+        System.out.printf("otherSiders(3)---> [ %.2f , %.2f ] %n",otherSiders(3)[1],otherSiders(3)[0]);
+    }
+}
